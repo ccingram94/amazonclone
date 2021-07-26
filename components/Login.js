da670c90a8c80c9d85d311e8feb3b8e9477d2375
Constancegit status <constanceingram94@gmail.com>
2021-07-26 12:39:28 -0500

    added login page with template for firebase integration

---
+++ components/Login.js
@@ -0,0 +1,47 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../styles/login.module.css'
+import Header from '../components/Header'
+import Prime from '../components/Prime'
+import Product from '../components/Product'
+import logo from '../public/amazontransparent.png'
+
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signIn = e => {
+        e.preventDefault();
+        //firebase login here
+    }
+
+    const register = e => {
+        e.preventDefault();
+        //firebase registration here
+    }
+
+  return (
+    <div className={styles.login}>
+        <Link href="/" className={styles.loginlogo}>
+            <Image src={ logo } alt="the amazon logo"></Image>
+        </Link>
+        <div className={logincontainer}>
+            <h1> Sign-In </h1>
+            <form>
+                <h5>E-Mail</h5>
+                <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
+
+                <h5>Password</h5>
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+
+                <button className={styles.signinbutton} onclick={signIn}>Sign In</button>
+            </form>
+            <p>
+                By signing-in you agree to the AMAZON FAKE CLONE conditions of use and sale.  Please see our Privacy Notice, our Cookies Notice, and our Interest-Based Ads Policy Notice.
+            </p>
+            <button className={styles.registerbutton} onClick={register}>Create your Amazon Account</button>
+        </div>
+      </div>
+  )
+}
---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
+import Prime from '../components/Prime'
+import Product from '../components/Product'
+
+export default function Login() {
+  return (
+    <div className={styles.container}>
+        <Image></Image>
+    </div>
+  )
+}
